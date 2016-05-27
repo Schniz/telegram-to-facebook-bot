701730515034bb526ceb2d3eee3a685bb33501f9
Gal Schlezinger <galstar@spitfire.co.il>
2016-05-27 08:43:53 +0300

    yay

--- bot.js
+++ bot.js
@@ -1,1 +1,44 @@
-console.log('o hai');
+import BotApi from 'telegram-bot-api';
+import getUrls from 'get-urls';
+import { Subject } from 'rx';
+import nodemailer from 'nodemailer';
+
+const {
+	BOT_TOKEN,
+	SMTP_SERVER,
+	FB_GROUP
+} = process.env;
+
+var transporter = nodemailer.createTransport(SMTP_SERVER);
+
+var mailOptions = {
+    from: '"Joe Hagever" <joe.hagever@gmail.com>', // sender address
+    to: `${FB_GROUP}@groups.facebook.com`, // list of receivers
+		subject: 'https://github.com', // Subject line
+    text: 'have fun', // plaintext body
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+});
+
+const head = arr => arr[0];
+const messages$ = new Subject();
+const toFullstack$ = messages$.filter(({ text }) => text.match(/\#fullstack/i));
+const texts$ = toFullstack$.pluck('text');
+const urls$ = texts$.flatMap(getUrls);
+
+const api = new BotApi({
+	token: process.env.BOT_TOKEN,
+	updates: {
+		enabled: true,
+	},
+});
+
+api.on('message', message => {
+	messages$.onNext(message);
+});
+
